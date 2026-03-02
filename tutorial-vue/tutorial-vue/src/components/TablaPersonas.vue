e56401230584dd6252f178f3f11797243248fdb7
rubenJ09 <rubenj.jimenez@estudiante.uam.es>
2026-03-02 12:35:30 +0100

    Apartado 1.9: Edición de personas en la tabla

--- tutorial-vue/tutorial-vue/src/components/TablaPersonas.vue
+++ tutorial-vue/tutorial-vue/src/components/TablaPersonas.vue
@@ -27,12 +27,24 @@
       </thead>
       <tbody>
         <tr v-for="(persona, indice) in personas" :key="persona.email">
-          <td>{{ persona.nombre }}</td>
-          <td>{{ persona.apellido }}</td>
-          <td>{{ persona.email }}</td>
-          <td>
-            <button @click="borrarPersona(indice)" class="btn btn-danger btn-sm">Borrar</button>
-          </td>
+          <template v-if="indiceEditando === indice">
+            <td><input v-model="editNombre" type="text" /></td>
+            <td><input v-model="editApellido" type="text" /></td>
+            <td><input v-model="editEmail" type="email" /></td>
+            <td>
+              <button @click="guardarEdicion(indice)" class="btn btn-success btn-sm">Guardar</button>
+              <button @click="cancelarEdicion" class="btn btn-secondary btn-sm">Cancelar</button>
+            </td>
+          </template>
+          <template v-else>
+            <td>{{ persona.nombre }}</td>
+            <td>{{ persona.apellido }}</td>
+            <td>{{ persona.email }}</td>
+            <td>
+              <button @click="editarPersona(indice)" class="btn btn-primary btn-sm">Editar</button>
+              <button @click="borrarPersona(indice)" class="btn btn-danger btn-sm">Borrar</button>
+            </td>
+          </template>
         </tr>
       </tbody>
     </table>
@@ -53,6 +65,11 @@ const nuevoApellido = ref('');
 const nuevoEmail = ref('');
 const mensajeError = ref('');
 const mensajeInfo = ref('');
+
+const indiceEditando = ref(null);
+const editNombre = ref('');
+const editApellido = ref('');
+const editEmail = ref('');
 
 defineOptions({
   name: 'tabla-personas',
@@ -78,6 +95,34 @@ function borrarPersona(indice) {
   personas.value.splice(indice, 1);
   mensajeInfo.value = 'Persona eliminada correctamente.';
   setTimeout(() => mensajeInfo.value = '', 2000);
+}
+
+function editarPersona(indice) {
+  indiceEditando.value = indice;
+  editNombre.value = personas.value[indice].nombre;
+  editApellido.value = personas.value[indice].apellido;
+  editEmail.value = personas.value[indice].email;
+}
+
+function guardarEdicion(indice) {
+  if (!editNombre.value || !editApellido.value || !editEmail.value) {
+    mensajeError.value = 'Todos los campos son obligatorios.';
+    return;
+  }
+  personas.value[indice] = {
+    nombre: editNombre.value,
+    apellido: editApellido.value,
+    email: editEmail.value
+  };
+  indiceEditando.value = null;
+  mensajeError.value = '';
+  mensajeInfo.value = 'Persona editada correctamente.';
+  setTimeout(() => mensajeInfo.value = '', 2000);
+}
+
+function cancelarEdicion() {
+  indiceEditando.value = null;
+  mensajeError.value = '';
 }
 </script>
 
