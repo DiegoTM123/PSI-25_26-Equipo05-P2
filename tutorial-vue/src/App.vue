9209abf80bef68be74ac5b148d8540eb99a236cc
DiegoTM123 <dterresmor@gmail.com>
2026-03-02 18:50:38 +0100

    Soy un vago, hecho hasta el 1.7

---
+++ tutorial-vue/src/App.vue
@@ -0,0 +1,65 @@
+<!-- App.vue -->
+<template>
+<div id="app" class="container">
+<div class="row">
+<div class="col-md-12">
+<h1>Personas</h1>
+</div>
+</div>
+<div class="row">
+<div class="col-md-12">
+<!-- Inclusion del componente "TablaPersonas" -->
+<formulario-persona @add-persona="agregarPersona"/> <!-- NUEVO -->
+<tabla-personas :personas="personas"/>
+</div>
+</div>
+</div>
+</template>
+<script setup>
+// Importacion del componente "TablaPersonas"
+import TablaPersonas from '@/components/TablaPersonas.vue'
+// NUEVO:
+import FormularioPersona from '@/components/FormularioPersona.vue'
+import { ref } from 'vue';
+// Declaracion de una variable reactiva "personas" usando "ref"
+const personas = ref([
+{
+id: 1,
+nombre: 'Jon',
+apellido: 'Nieve',
+email: 'jon@email.com',
+},
+{
+id: 2,
+nombre: 'Tyrion',
+apellido: 'Lannister',
+email: 'tyrion@email.com',
+},
+{
+id: 3,
+nombre: 'Daenerys',
+apellido: 'Targaryen',
+email: 'daenerys@email.com',
+},
+]);
+
+// definimos una funcion de nombre agregarPersona que agrega una nueva persona al array, tmb hace el ID único
+const agregarPersona = (persona) => {
+let id = 0;
+if (personas.value.length > 0) {
+id = personas.value[personas.value.length - 1].id + 1;
+}
+personas.value = [...personas.value, { ...persona, id }];
+};
+
+
+</script>
+<style>
+/* Estilos globales para todos los elementos button en la aplicacion */
+button {
+background: #009435;
+border: 1px solid #009435;
+}
+</style>
+
+
---
+++ tutorial-vue/src/components/TablaPersonas.vue
@@ -0,0 +1,40 @@
+<!-- src/components/TablaPersonas.vue -->
+<template>
+<!-- Contenedor principal del componente -->
+<div id="tabla-personas">
+<!-- Tabla para mostrar informacion de personas -->
+<table class="table">
+<!-- Encabezado de la tabla -->
+<thead>
+<tr>
+<!-- Columnas del encabezado -->
+<th>Nombre</th>
+<th>Apellido</th>
+<th>Email</th>
+</tr>
+</thead>
+<!-- Cuerpo de la tabla -->
+<tbody>
+  <tr v-for="persona in personas" :key="persona.id">
+    <td>{{ persona.nombre }}</td>
+    <td>{{ persona.apellido }}</td>
+    <td>{{ persona.email }}</td>
+  </tr>
+</tbody>
+</table>
+</div>
+</template>
+<script setup>
+// Definicion del componente Vue
+defineOptions({
+// Nombre del componente
+name: 'tabla-personas',
+});
+const props = defineProps({
+personas: {type: Array, default: () => []},
+});
+
+</script>
+<style scoped>
+/* Estilos especificos del componente con el modificador "scoped" */
+</style>
