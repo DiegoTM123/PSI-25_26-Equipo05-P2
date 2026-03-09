9dc5704be3059fa4beb6e7eb3feea2439574f268
DiegoTM123 <dterresmor@gmail.com>
2026-03-09 10:11:57 +0100

    BE comletado

--- persona/api/models.py
+++ persona/api/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
 class Persona(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    
-    def __str__(self):
-        return f"{self.nombre} {self.apellido}"
-    
+    email = models.EmailField()
+
     class Meta:
-        ordering = ['apellido', 'nombre']
+        ordering = ['id']
+        
